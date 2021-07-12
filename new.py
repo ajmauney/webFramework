ec0b1562b645ba981ef59c10fef25a5a3a4ba90a
ajmauney <86627968+ajmauney@users.noreply.github.com>
2021-07-12 16:30:23 -0400

    Update new.py

--- new.py
+++ new.py
@@ -1,5 +1,22 @@
 from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd7c2cac4b6e388eaf69012b25c3fd6f8'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
 
 @app.route("/")
 @app.route("/home")
@@ -9,6 +26,18 @@ def home():
 @app.route("/new")
 def new():
   return render_template('new.html', subtitle='Second Page', text = 'This is our second page')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+      user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+      db.session.add(user)
+      db.session.commit()
+      flash(f'Account created for {form.username.data}!', 'success')
+      return redirect(url_for('home')) # if so - send to home page
+    
+    return render_template('register.html', title='Register', form=form)
 
 
 if __name__ == '__main__':
